680f31c065cee049cd554001f04ab5bb4c5594a5
gabriel.trujillo <gabrieltrujillotb@gmail.com>
2025-03-12 19:57:19 +0000

    Implementation of database connection

---
+++ wsbuilder/src/main/java/com/project/wsbuilder/dbconnection/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package com.project.wsbuilder.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseConnection implements AutoCloseable {
+    private static final String URL = "jdbc:postgresql://localhost:3007/wsbuilder";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+
+    public DatabaseConnection() throws SQLException {
+        this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(query);
+        return statement.executeQuery();
+    }
+
+    public int executeUpdate(String query) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(query);
+        return statement.executeUpdate();
+    }
+
+    @Override
+    public void close() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
