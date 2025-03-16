492de11f0136799fdc96023b2113c248868b9f81
gabriel.trujillo <gabrieltrujillotb@gmail.com>
2025-03-16 19:18:25 +0000

    Add vscode api call to show the content based on rating daily and download quantity

--- wsbuilder/src/main/java/module-info.java
+++ wsbuilder/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.project.wsbuilder {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.json;
 
     opens com.project.wsbuilder.controllers to javafx.fxml;
     exports com.project.wsbuilder;
